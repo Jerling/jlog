cd42d32a65b865019eda9002518b66528f74456b
李林杰 <1161676899@qq.com>
2019-04-20 11:52:46 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+INSTALL_DIR=/usr/local
+SRC_DIR=jlog
+BIULD_DIR=build
+
+if [ "$1" == "uninstall" ]
+then
+    sudo rm -rf $INSTALL_DIR/bin/jlogd
+    sudo rm -rf $INSTALL_DIR/include/jlog*
+    sudo rm -rf $INSTALL_DIR/lib/*jlog*
+    exit
+fi
+if [ ! -d BUILD_DIR ]
+then
+    mkdir BUILD_DIR
+fi
+
+cd BUILD_DIR && cmake .. && make && cd -
+
+if [ "$1" == "install" ]
+then
+    if [ ! -d $INSTALL_DIR/include/$SRC_DIR ]
+    then
+        sudo mkdir $INSTALL_DIR/include/$SRC_DIR
+    fi
+    sudo cp $SRC_DIR/*.h $INSTALL_DIR/include/$SRC_DIR
+    sudo cp jlog.h $INSTALL_DIR/include
+    sudo cp $BIULD_DIR/lib/libjlog.so $INSTALL_DIR/lib
+    sudo cp $BIULD_DIR/bin/jlogd $INSTALL_DIR/bin
+fi
